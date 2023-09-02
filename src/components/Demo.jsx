fe1b3ab2580fbd88cb9a696398920116013e2fc1
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-02 07:42:12 +0530

    Demo structure

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { copy, link, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-    <div>Demo</div>
-  )
-}
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center"></form>
+      </div>
 
-export default Demo
+    </section>
+  )
+};
+
+export default Demo;
