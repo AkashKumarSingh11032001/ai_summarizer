3d2a018f34d0fae2461d20e40185b27b2559d1af
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-02 11:34:10 +0530

    ./

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-3 pt-3">
         <img src={logo} alt="sumz_logo" className='"w-28 object-contain' />
-        <button type="button" onClick={() => window.open("")} className="black_btn">
+        <button type="button" onClick={() => window.open("https://github.com/AkashKumarSingh11032001/ai_summarizer")} className="black_btn">
           GitHub
         </button>
       </nav>
