a9a45b1a98b4f74fbb45bc43226bea13fbab9be7
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-02 09:14:27 +0530

    Demo structure

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,11 +3,22 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         {/* Input search */}
-        <form className="relative flex justify-center items-center">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
           <img
             src={linkIcon}
             alt="link-icon"
@@ -17,8 +28,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Paste the article link"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
