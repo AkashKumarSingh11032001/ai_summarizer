3e7cb788bd60be5af382c956e4853de4a4fde213
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-02 07:16:51 +0530

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-3 pt-3">
+        <img src={logo} alt="sumz_logo" className='"w-28 object-contain' />
+        <button type="button" onClick={() => window.open("")} className="black_btn">
+          GitHub
+        </button>
+      </nav>
 
-export default Hero
+      <h1 className="head_text">
+        Summerize Article with <br/>
+        <span className="orange_gradient">OpenAI GPT</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Hero;
