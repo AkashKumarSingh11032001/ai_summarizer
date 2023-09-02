35693cd9ac16ca739a5c144f29bcd22e6ab7e3d8
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-02 09:38:10 +0530

    redux store setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
