1fc29387f24a37a761ac7f7952fabd06de75943a
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-09-02 08:56:35 +0530

    input section done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,21 +6,33 @@ const Demo = () => {
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center"></form>
+        {/* Input search */}
+        <form className="relative flex justify-center items-center">
+          <img
+            src={linkIcon}
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Paste the article link"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            <p>↵</p>
+          </button>
+        </form>
+        {/* Browse URL History */}
       </div>
-      <img
-        src={linkIcon}
-        alt="link_icon"
-        className="absolute left-0 my-2 ml-3 w-5"
-      />
-      <input
-        type="url"
-        placeholder="Enter a URL"
-        value=""
-        onChange={() => {}}
-        required
-        className="url_input peer"
-      />
+
+      {/* Display Result */}
     </section>
   );
 };
